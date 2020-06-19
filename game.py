e377bf520b220c02918ccf34341dec0f0fc2265e
NeilRajah <neil.balaskandarajah@gmail.com>
2020-06-19 02:16:54 -0400

    Rough AI implementation, works in some cases
    - gets stuck in repeating loops
    - no backtracking yet
    
    - implement scoring for choosing best remaining move
    - implement backtracking when stuck

--- game.py
+++ game.py
@@ -4,44 +4,67 @@ Author: Neil Balaskandarajah
 Created on: 18/06/2020
 Game class with all interactions
 """
+from itertools import permutations
+from copy import deepcopy
 
 class Game():
     def __init__(self, num_rings):
         """
         Create a game by declaring its max size
         """
-        self.num_rings = num_rings
-        self.stacks = dict()
-        self.STACK_LABELS = 'ABCDEFGH'
-        self.label_idx = 0
+        self.num_pieces = num_rings         #Max number of pieces on a stack
+        self.stacks = dict()                #Stacks containing pieces
+        self.STACK_LABELS = 'ABCDEFGH'      #Alphabetical labels for each stack
+        self.label_idx = 0                  #Index of the current label for adding stakcs
+        self.history = []                   #Move history
+        self.prev_move = ''                 #Previous move
 
     def add_stack(self, stack):
         """
-        Add a stack to the game
-        return - Information on stack added
+        Add a stack to the game, ordered top to bottom (ie. index 0 is top, index -1 is bottom)
         """
         if self.label_idx > len(self.STACK_LABELS):
             raise Exception('Cannot have more than {} stacks!'.format(len(self.STACK_LABELS)))
         if not isinstance(stack, list):
             raise Exception('Stack must be a Python list!')
+        
+        stack.reverse()
         self.stacks[self.STACK_LABELS[self.label_idx]] = stack
         self.label_idx += 1
         return "Stack {} added: {}".format(self.STACK_LABELS[self.label_idx-1], stack)
 
-    def move_piece(self, pair_tup):
+    def move_pieces(self, pair_tup):
         """
         Move a piece from the top of one stack to the other
         """
+        if not self.is_pair_compatible(pair_tup):
+            raise Exception("Stacks {} and {} are not compatible!".format(pair_tup[0], pair_tup[1]))
         a = self.stacks[pair_tup[0]]
         b = self.stacks[pair_tup[1]]
         b.append(a.pop())
+        self.history.append(pair_tup)
+        self.prev_move = pair_tup
+        return "Moved {} from {} to {}".format(b[-1], pair_tup[0], pair_tup[1])
+
+    def is_pair_compatible(self, pair_tup):
+        """
+        Check if a pair of stacks are compatible
+        """
+        a = self.stacks[pair_tup[0]]
+        b = self.stacks[pair_tup[1]]
+
+        if len(a) == 0: return False
+        if len(b) == 0: return True
+        if len(b) == self.num_pieces: return False
+        if a[-1] == b[-1]: return True
+        return False
 
     def display(self):
         """
         Print the game out to the console
         """
-        print('-' * (len(self.stacks) + 2))
-        for i in reversed(range(self.num_rings)):
+        print('-' * (len(self.stacks) + 4))
+        for i in reversed(range(self.num_pieces)):
             row = ""
             for lbl in self.stacks:
                 if i > len(self.stacks[lbl])-1:
@@ -49,5 +72,94 @@ class Game():
                 else:
                     row = row + "{} ".format(self.stacks[lbl][i])
             print(row)
-        print('-' * (len(self.stacks) + 2))
+        print('-' * (len(self.stacks) + 4))
 
+    def display_history(self):
+        """
+        Print the move history to the console
+        """
+        out = "{}: ".format(len(self.history))
+        for move in self.history:
+            for stack_label in move:
+                out = out + stack_label
+            out = out + " "
+        print(out)
+
+    def move_and_display(self, pair_tup):
+        """
+        Move a piece and print game information to the console
+        """
+        self.move_pieces(pair_tup)
+        self.display()
+        self.display_history()
+
+    def is_solved(self):
+        """
+        Return if the game is solved or not
+        """
+        for stack in self.stacks:
+            if not self.is_stack_solved_or_empty(stack):
+                return False
+        return True
+
+    def is_stack_solved_or_empty(self, lbl):
+        """
+        Return if the stack is solved or not
+        """
+        return len(self.stacks[lbl]) == 0 or \
+            (len(set(self.stacks[lbl])) == 1 and len(self.stacks[lbl]) == self.num_pieces)
+
+    def solve(self):
+        """
+        Solve the puzzle
+        """
+        print("*******START*******")
+
+        #Start the solution by calculating all possible moves
+        self.moves = list(permutations(self.stacks, 2))
+
+        num_loops = 50
+        loop = 0
+        while not self.is_solved():
+            if loop >= num_loops: print("too many loops"); break
+
+            #Create a copy of the moves for this iteration
+            pairs = deepcopy(self.moves)
+
+            #Check for empty or solved stacks
+            solved_empty = []
+            for stack in self.stacks:
+                if self.is_stack_solved_or_empty(stack):
+                    solved_empty.append(stack)
+
+            #Eliminate all moves with pieces going from empty or solved pairs
+            remove = list()
+            for pair in pairs:
+                for stack in solved_empty:
+                    if pair[0] == stack:
+                        remove.append(pair)
+            pairs = [item for item in pairs if item not in remove] #subtract the lists
+
+            #Eliminate the move that is the opposite as the previous move
+            if (self.prev_move != ''):
+                for pair in pairs:
+                    if pair[0] == self.prev_move[1] and pair[1] == self.prev_move[0]:
+                        pairs.remove(pair)
+                        break
+
+            #Eliminate all moves between incompatible stacks
+            remove.clear()
+            for pair in pairs:
+                if not self.is_pair_compatible(pair):
+                    remove.append(pair)
+            pairs = [item for item in pairs if item not in remove]
+
+            #Move the piece at the first of the moves left
+            self.move_pieces(pairs[0])
+
+            loop += 1
+        if self.is_solved():
+            print("*******SOLVED*******")
+        self.display()
+        self.display_history()
+        
--- main.py
+++ main.py
@@ -5,50 +5,99 @@ Created on: 17/06/2020
 AI for solving hoop stack game
 """
 from game import Game
-from copy import deepcopy
-from itertools import permutations
 
-def is_solved(maxSize, stacks):
-    """Check if all stacks are solved"""
-    for lis in stacks:
-        if len(lis) != 0 and len(lis) != maxSize:
-            return False
-    return True
+#lvl 1
+# game = Game(3)
+# print(game.add_stack([1]))
+# print(game.add_stack([1, 1]))
 
-def can_move(a, b):
-    """If top of stacks are same, or if b is empty"""
-    return not len(a) == 0 and (len(b) == 0 or a[-1] == b[-1])
+#lvl 2
+# game = Game(3)
+# print(game.add_stack([1, 2, 2]))
+# print(game.add_stack([1, 1, 2]))
+# print(game.add_stack([]))
 
-def solve(maxSize, s):
-    all_moves = list(permutations(s, 2))
-    print (all_moves)
+#lvl 3
+# game = Game(3)
+# print(game.add_stack([1, 2, 1]))
+# print(game.add_stack([]))
+# print(game.add_stack([1, 2, 2]))
 
-    # if perms.__contains__(('A', 'C')):
-    #     print("yes")
-    # removals = set()
-    # for p in perms:
-    #     if 'A' in p:
-    #         removals.add(p)
-    # perms = perms - removals 
-    # print(perms)
-    
-#------------------------Main Script------------------------#
+#lvl 4
+# game = Game(4)
+# print(game.add_stack([1, 1, 2, 1]))
+# print(game.add_stack([2, 2, 1, 2]))
+# print(game.add_stack([]))
+# print(game.add_stack([]))
 
-#Create stacks
-# s = dict()
-# maxSize = 3
-# s['A'] = [1, 2, 2]
-# s['B'] = [2, 2, 1]
-# s['C'] = []
-# so = deepcopy(s) #for printing after
- 
-# solve(maxSize, s)
+#lvl 5, stuck in repeating loop with red hoops
+# game = Game(5)
+# print(game.add_stack([1, 2, 1, 1, 2]))
+# print(game.add_stack([]))
+# print(game.add_stack([2, 1, 2, 1, 2]))
+# print(game.add_stack([]))
 
-game = Game(3)
-print(game.add_stack([1, 2, 2]))
-print(game.add_stack([2, 2, 1]))
-print(game.add_stack([]))
+#lvl 6
+# game = Game(3)
+# print(game.add_stack([1, 2, 3]))
+# print(game.add_stack([1, 3, 2]))
+# print(game.add_stack([2, 1, 3]))
+# print(game.add_stack([]))
+# print(game.add_stack([]))
+
+#lvl 7, stuck in classic 3-way repetition
+# game = Game(3)
+# print(game.add_stack([1, 2, 3]))
+# print(game.add_stack([]))
+# print(game.add_stack([3, 1, 2]))
+# print(game.add_stack([3, 2, 1]))
+# print(game.add_stack([]))
+
+#lvl 8
+# game = Game(4)
+# print(game.add_stack([1, 1, 2, 1]))
+# print(game.add_stack([2, 1, 2, 2]))
+# print(game.add_stack([]))
+# print(game.add_stack([]))
+
+#lvl 9, stuck in repeating loop with green hoop
+# game = Game(5)
+# print(game.add_stack([1, 2, 2, 1, 2]))
+# print(game.add_stack([]))
+# print(game.add_stack([1, 1, 2, 2, 1]))
+# print(game.add_stack([]))
+
+#lvl 10, stuck in deep repeating loop
+# game = Game(3)
+# print(game.add_stack([]))
+# print(game.add_stack([1, 2, 2]))
+# print(game.add_stack([2, 1, 3]))
+# print(game.add_stack([3, 1, 3]))
+# print(game.add_stack([]))
+
+#lvl 11, hit deadlock
+# game = Game(3)
+# print(game.add_stack([1, 2]))
+# print(game.add_stack([2, 3, 4]))
+# print(game.add_stack([3, 5, 4]))
+# print(game.add_stack([1]))
+# print(game.add_stack([4, 5, 5]))
+
+#lvl 12
+game = Game(4)
+print(game.add_stack([1, 2, 3]))
+print(game.add_stack([2, 1, 1]))
+print(game.add_stack([3, 2]))
+print(game.add_stack([3, 1, 2, 3]))
+
+#lvl 13, hoops repeatedly moving to empty stack
+game = Game(5)
+print(game.add_stack([1,2,1,3,2]))
+print(game.add_stack([4]))
+print(game.add_stack([3]))
+print(game.add_stack([4,3,4,1]))
+print(game.add_stack([3,4,2,1,1]))
+print(game.add_stack([4,2,2,3]))
+
 game.display()
-
-game.move_piece(('A', 'C'))
-game.display()
+game.solve()
