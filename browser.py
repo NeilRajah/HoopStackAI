affbcada2be679333bdd77dc4860d167ce222a71
NeilRajah <neil.balaskandarajah@gmail.com>
2020-06-22 16:14:26 -0400

    Can get bounding box of stack
    - stack location done
    - need to add colors (separate filtering)

--- browser.py
+++ browser.py
@@ -49,11 +49,74 @@ def _pair_click(stack_locations, pair):
     x, y = stack_locations[pair[1]]; pag.moveTo(x, y, moveTime); pag.click()
     sleep(delay)
 
-def _game_from_image(img):
+def _game_from_image(file):
     """
     Return a Game created with an image
     """
+    #Get the image from the file
+    img = cv2.imread(file, cv2.IMREAD_COLOR)
+    # cv2.imshow('img', img)
 
+    #Filter out the background
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  # Convert BGR to HSV
+    lower_clr = np.array([60, 0, 20]); upper_clr = np.array([255, 255, 255])
+    no_bg = cv2.inRange(hsv, lower_clr, upper_clr)
+
+    #filter out to black and white
+    filtered = cv2.bitwise_and(no_bg, no_bg, mask=no_bg)
+    # cv2.imshow('filtered', filtered)
+
+    #Blur the image so each stack becomes an individual contour
+    median = cv2.medianBlur(filtered, 21)
+    # cv2.imshow('median', median)
+
+    contours, _ = cv2.findContours(median, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  #can only take black and white
+    # img = cv2.drawContours(img, cnt, -1, (0,255,0), 3)
+
+    STACK_LABELS = 'ABCDEFG'; idx = 0
+
+    #get all the bounding rectangles for the contours
+    rects = [cv2.boundingRect(c) for c in contours];
+
+    #Draw on the image
+    for x,y,w,h in rects:
+        #draw bounding rectangle and its label
+        img = cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,0), 2)
+        img = cv2.putText(img, STACK_LABELS[idx], (x,y), cv2.FONT_HERSHEY_DUPLEX, 1, (255,0,0), 3)
+        idx += 1
+
+        #draw the centers of each bounding rect
+        # x_c = x + w//2; y_c = y + h//2
+        # img = cv2.circle(img, (x_c, y_c), 5, (0, 0, 0), -1)
+
+        #draw potential stack points
+        n = 6; h_step = h//n
+        for i in range(1, n):
+            img = cv2.circle(img, (x + w//2, y + h_step*i), 5, (0,0,0), -1)
+
+    #resize image to show
+    s = 3; size_dim = (int(img.shape[1]*s), int(img.shape[0]*s))
+    img = cv2.resize(img, size_dim)
+    cv2.imshow('with contours', img)
+
+    # def nothing(x): pass
+
+    # win_name = 'finding contours'
+    # cv2.namedWindow(win_name)
+    # cv2.createTrackbar('bot', win_name, 0, 255, nothing)
+    # cv2.createTrackbar('top', win_name, 0, 255, nothing)
+    #
+    # while 1:
+    #     cv2.imshow(win_name, mask)
+    #     k = cv2.waitKey(1) & 0xFF
+    #     if k == 27:
+    #         break
+    #
+    #     bot = cv2.getTrackbarPos('bot', win_name)
+    #     top = cv2.getTrackbarPos('top', win_name)
+    #     ret, mask = cv2.threshold(median, bot, top,cv2.THRESH_BINARY)
+
+    cv2.waitKey(0); cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     #Create the game
@@ -65,15 +128,13 @@ if __name__ == '__main__':
     game.add_stack([5, 2, 6, 3, 1])
     game.add_stack([2, 6, 3, 6, 6])
     game.add_stack([5, 4, 3, 1, 1])
-    game.solve()
+    # game.solve()
 
     #Set the (x,y) coordinates of each stack
     stack_locations6 = {'A': (2049, 1131), 'B': (2213, 1121), 'C': (2430, 1097), 'D': (2033, 1379), 'E': (2274, 1372), 'F': (2436, 1406)} #6
     stack_locations7 = {'A': (3906, -1210), 'B': (4075, -1201), 'C': (4248, -1213), 'D': (3769, -958), 'E': (3958, -954),
                        'F': (4179, -938), 'G': (4377, -965)}
 
-    try:
-        _play_game(game, stack_locations6)
-    except KeyboardInterrupt:
-        print('keyboard exit')
+    # _play_game(game, stack_locations6)
     # _get_click_locations()
+    _game_from_image('lvl58.png')
--- cv_test.py
+++ cv_test.py
@@ -10,9 +10,9 @@ from game import Game
 
 #load the image in
 img = cv2.imread('lvl58.png', cv2.IMREAD_COLOR)
-cv2.imshow('regular', img)
+# cv2.imshow('regular', img)
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  #convert regular BRG img to gray
-cv2.imshow('grayscale', img_gray)
+# cv2.imshow('grayscale', img_gray)
 
 #Drawing on images
 # cv2.line(img, (0,0), (100,100), (0, 0, 255), 5)  #line
@@ -38,7 +38,7 @@ roi = img[100:150, 100:150]
 #Thresholding
 rows, cols, channels = img.shape
 roi = img[0:rows, 0:cols]
-ret, mask = cv2.threshold(img_gray, 140, 255, cv2.THRESH_BINARY_INV)  #if pixel value above 220, conv to 0, below to 255
+ret, mask = cv2.threshold(img_gray, 140, 255, cv2.THRESH_BINARY_INV)  #if pixel value above a, conv to 0, below to 255
 # cv2.imshow('invbin', mask)
 
 gaus = cv2.adaptiveThreshold(img_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 115, 1)
@@ -46,10 +46,63 @@ gaus = cv2.adaptiveThreshold(img_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.
 
 #Color filtering
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  #Convert BGR to HSV
-lower_clr = np.array([50, 0, 190]); upper_clr = np.array([255, 255, 255])  #Top and bottom bounds in HSV
+lower_clr = np.array([60, 30, 20]); upper_clr = np.array([255, 255, 255])  #Top and bottom bounds in HSV
+#tune H for specific color
+#tune S for how vibrant/faded it is
+#tune V for how light/dark color is
 
 mask = cv2.inRange(hsv, lower_clr, upper_clr)  #pixels in the range of lower and upper
-res = cv2.bitwise_not(img, img_gray, mask=mask)
-cv2.imshow('filtered', res)
+filtered = cv2.bitwise_and(img, img, mask=mask)
+cv2.imshow('filtered', filtered)
+
+#Blurring and Smoothing
+kx = ky = 8 #reduces spots
+kernel = np.ones((kx, ky), np.float32) / (kx * ky)  #array of ones
+# smoothed = cv2.filter2D(filtered, -1, kernel)
+# cv2.imshow('smoothed', smoothed)
+
+bx = by = 5  #doesn't work with even numbers
+# blur = cv2.GaussianBlur(filtered, (bx, by), 0)
+# cv2.imshow('blur', blur)
+
+m = 7  #doesn't work with even numbers, seems like good choice
+median = cv2.medianBlur(filtered, m)
+cv2.imshow('median', median)  #solid color
+
+b1 = 75; b2 = b3 = 100  #decent image quality but slow
+# bilat = cv2.bilateralFilter(filtered, b1, b2, b2)
+# cv2.imshow('bilat', bilat)
+
+#Morphological Transformations
+morph_kernel = np.ones((3,3), np.uint8)  #larger the tuple, larger the area for erosion check
+
+# dilation = cv2.dilate(filtered, morph_kernel, iterations=1)  #makes colored parts bigger
+# cv2.imshow('dilation', dilation)
+
+# ero_dilat = cv2.erode(dilation, morph_kernel, iterations=1)  #compound (get rid of edges, make insides bigger)
+# cv2.imshow('ero_dilat', ero_dilat)
+
+# opening = cv2.morphologyEx(filtered, cv2.MORPH_OPEN, kernel); cv2.imshow('opening', opening)
+# closing = cv2.morphologyEx(filtered, cv2.MORPH_CLOSE, kernel); cv2.imshow('closing', closing)
+
+#Edge Detection and Gradients
+# lap_dilat = cv2.Laplacian(dilation, cv2.CV_64F); cv2.imshow('Laplacian_dilation', lap_dilat)
+# cv2.imshow('sobelx', cv2.Sobel(filtered, cv2.CV_64F, 1, 0, ksize=5))
+# cv2.imshow('sobely', cv2.Sobel(filtered, cv2.CV_64F, 0, 1, ksize=5))
+edge = 100; edge_img = cv2.Canny(median, edge, edge)
+# cv2.imshow('edges', edge_img)
+
+#contours
+cont_img, contours = cv2.findContours(edge_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+contours = np.array(contours).reshape((-1,1,2)).astype(np.int32)
+cv2.drawContours(img, [contours], -1, (0, 255, 0), 3)
+# cv2.imshow('contours', img)
+
+# imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+
+contours, hierarchy = cv2.findContours(edge_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(img, contours, -1, (0,255,0), 3)
+cv2.imshow('contours', img)
 
 cv2.waitKey(0); cv2.destroyAllWindows()
