fdcc48f62c441bb521f4a891ae1974aa751a139d
NeilRajah <neil.balaskandarajah@gmail.com>
2020-06-25 03:32:44 -0400

    Still working on creating stacks
    - might get individual colors and filter them out
      - a contour of each piece would be ideal, easy to get the avg color

--- image_filtering.py
+++ image_filtering.py
@@ -9,6 +9,7 @@ import cv2
 import numpy as np
 from os import listdir
 from copy import deepcopy
+from matplotlib import pyplot as plt
 
 def scale_image(image, s):
     """
@@ -69,34 +70,64 @@ def game_from_browser(filename, display_image=False):
     Create the game from an image from the browser
     """
     #Get the image and scale it
-    img = cv2.imread(filename, cv2.COLOR_RGB2BGR)
+    img = cv2.imread(filename, cv2.IMREAD_COLOR)
     img = scale_image(img, 1); orig = img
 
-    #Get the bounds of each stack
-    stack_bounds = find_stacks(img)
-
-    #Create smaller images for each stack
-    stacks = [img[y:y+h, x:x+w] for x,y,w,h in stack_bounds.values()]
-    # stacks = []
-    # for x,y,w,h in stack_bounds.values():
-    #     mid_x = x+w//2; mid_y = y+h//2
-    #     stacks.append(img[mid_x:mid_x+1, mid_y:mid_y+1])
-    #     stacks[-1] = scale_image(stacks[-1], 5)
-
-    #Show the smaller images
-    for i,stack in enumerate(stacks):
-        stack_mask = filter_bg(stack, lower_clr=np.array([30,0,10]))
-        # stack = cv2.bitwise_and(stack, stack, mask=stack_mask)
-        # e = 7; stack = cv2.erode(stack, np.ones((e,e), np.uint8))
-        stack = cv2.Canny(stack, 200, 50)
-        o = 2; stack = cv2.morphologyEx(stack, cv2.MORPH_CLOSE, np.ones((o,o), np.uint8))
-        cv2.imshow('stack{}'.format(i), stack)
-
-    cv2.waitKey(0); cv2.destroyAllWindows()
     #Show the image
     if display_image:
         cv2.imshow('browser_game', img)
         cv2.waitKey(0); cv2.destroyAllWindows()
 
+    #Get the bounds of each stack
+    stack_bounds = find_stacks(img)
+
+    #Create smaller images for each stack
+    stacks = [scale_image(img[y:y+h, x:x+w], 1) for x,y,w,h in stack_bounds.values()]
+
+    #Filter the smaller images
+    for i,stack in enumerate(stacks):
+        #Remove the background
+        stack_mask = filter_bg(stack, lower_clr=np.array([30,0,10]))
+        stacks[i] = cv2.bitwise_and(stack, stack, mask=stack_mask)
+        # cv2.imshow('stack', stacks[i]); cv2.waitKey(0)
+    game_stacks = [get_game_stack(stack) for stack in stacks]
+
+    cv2.waitKey(0); cv2.destroyAllWindows()
+
+def get_game_stack(stack):
+    """
+    Create a stack from the game with the sub-image of that stack
+    """
+    #Blur out and convert to HSV
+    game_stacks = []
+    stack = cv2.medianBlur(stack, 19)  #; orig = stack
+    stack = cv2.cvtColor(stack, cv2.COLOR_BGR2HSV)
+
+    #Search down a strip of pixels to distinct colors (H values)
+    x = int(stack.shape[1] * 0.25); colors = []
+    for y in range(stack.shape[0]):
+        h,s,v = stack[y,x]
+        #Get non-black or very dark colors
+        if v > 20:  #[0,255], 20 is ~9 on [0,100]
+            colors.append(h)
+
+    if len(colors) > 0: plt.plot(colors); plt.show()
+
+    cv2.waitKey(0); cv2.destroyAllWindows()
+
+def _split_list(lst, n):
+    """
+    Split the list lst into n-sized chunks
+    """
+    if len(lst) == 0: return []
+    n = len(lst) // n
+    return [lst[i:i + n] for i in range(0, len(lst), n)]
+
+def _avg(lst):
+    """
+    Return the average of a list
+    """
+    return sum(lst) / len(lst)
+
 if __name__ == '__main__':
-    game_from_browser('lvl2.png', display_image=False)
+    game_from_browser('lvl3.png', display_image=False)
