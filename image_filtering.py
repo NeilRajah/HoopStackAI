46c48a9272424b6feba9017fa1e61c4e04fbf8f2
NeilRajah <neil.balaskandarajah@gmail.com>
2020-06-27 02:03:41 -0400

    Better testing
    - added GUI for quicker tuning
    - added layered system for filters
    - added tests for each layer, better overall performance
    - making progress with splitting stack into pieces

--- image_filtering.py
+++ image_filtering.py
@@ -11,76 +11,19 @@ from os import listdir
 from copy import deepcopy
 from matplotlib import pyplot as plt
 
+def draw_rect(image, rect):
+    """
+    Draw a rectangle onto image based on its top left (x,y) and width and height
+    """
+    x,y,w,h = rect
+    return cv2.rectangle(image, (x,y), (x+w,y+h), [0,255,0], 2)
+
 def scale_image(image, s):
     """
     Scale image by a factor of s
     """
     w = int(image.shape[1] * s); h = int(image.shape[0] * s)
     return cv2.resize(image, (w, h))
-
-def filter_bg(img, lower_clr=None):
-    """
-    Filter out the background of an image of the game
-    """
-    # Convert to HSV
-    if lower_clr is None:
-        lower_clr = np.array([15, 0, 10])
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    # Filter out the background color
-    upper_clr = np.array([255, 255, 255])
-    img = cv2.inRange(hsv, lower_clr, upper_clr)
-
-    # Remove small spots to make stacks uniform
-    k = 11; kernel = np.ones((k, k), np.uint8)
-    img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
-    img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
-
-    return img
-
-def find_stacks(img, show=False):
-    """
-    Find the stacks in the image
-    """
-    if show: orig = deepcopy(img)
-    img = filter_bg(img)
-
-    #Find the contours in the image
-    contours, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    #Find the bounding rectangle for each contour and get its center
-    rects = [cv2.boundingRect(c) for c in contours]
-    stacks = dict(); STACK_LABELS = 'ABCDEFGH'; idx = 0; all_stacks = []
-    for r in rects:
-        stacks[STACK_LABELS[idx]] = r
-        idx += 1
-
-    #Show the stacks
-    if show:
-        for x,y,w,h in rects:
-            orig = cv2.rectangle(orig, (x,y), (x+w,y+h), 255, 3)
-        orig = scale_image(orig, 0.5)
-        cv2.imshow('stacks', orig)
-        cv2.waitKey(0); cv2.destroyAllWindows()
-
-    #Return the rectangles bounding each stack
-    return stacks
-
-def stack_subimages(image, stack_bounds):
-    """
-    Create the subimages of a stack given an image
-    """
-    stacks = [image[y:y + h, x:x + w] for x, y, w, h in stack_bounds]
-    # for stack in stacks:
-    #     cv2.imshow('stack', scale_image(stack, 2))
-    #     cv2.waitKey(0)
-
-    # Filter the smaller images
-    for i, stack in enumerate(stacks):
-        # Remove the background
-        stack_mask = filter_bg(stack, lower_clr=np.array([18, 0, 0]))  # H below 18 starts to get the holder
-        stacks[i] = cv2.bitwise_and(stack, stack, mask=stack_mask)
-    return stacks
 
 def game_from_browser(filename):
     """
@@ -100,62 +43,6 @@ def game_from_browser(filename):
 
     cv2.imshow('original', orig); cv2.imshow('stack_bg', stack_bg)
     cv2.waitKey(0); cv2.destroyAllWindows()
-
-def get_game_stack(stack):
-    """
-    Create a stack from the game with the sub-image of that stack
-    """
-    #Blur out and convert to HSV
-    game_stacks = []
-    # stack = cv2.cvtColor(stack, cv2.COLOR_BGR2HSV)
-    # e = 5; stack = cv2.erode(stack, np.ones((e,e), np.uint8))
-    # a = 13; sigmaColor = sigmaSpace = 1000
-    # stack = cv2.bilateralFilter(stack, a, sigmaColor, sigmaSpace)
-    # stack = cv2.medianBlur(stack, 5)
-
-    #Add the unique colors of the stack
-    x = int(stack.shape[1] * 0.75); colors = []
-    for y in range(stack.shape[0]):
-        px = stack[y,x]
-        if px[2] > 20 and px[1] > 100:
-            colors.append(px)
-
-    #Visualize the colors by creating an image of the colors
-    if len(colors) > 0:
-        height = 50
-        color_img = np.zeros((height, len(colors), 3), np.uint8)
-        for x,clr in enumerate(colors):
-            for y in range(height):
-                color_img[y,x] = clr
-        color_img = cv2.cvtColor(color_img, cv2.COLOR_BGR2HSV)  #Convert to HSV
-        color_img = cv2.Canny(color_img, 50, 50)
-        cv2.imshow('color_img', scale_image(color_img, 2))
-
-    print("{}: {}".format(len(colors), colors))
-    cv2.imshow('stack', scale_image(stack, 2))
-
-    cv2.waitKey(0); cv2.destroyAllWindows()
-
-def unique_colors(stack):
-    """
-    Get the unique colors in an image
-    """
-    uniques = np.unique(stack, axis=0)
-    print(uniques)
-
-def _split_list(lst, n):
-    """
-    Split the list lst into n-sized chunks
-    """
-    if len(lst) == 0: return []
-    n = len(lst) // n
-    return [lst[i:i + n] for i in range(0, len(lst), n)]
-
-def _avg(lst):
-    """
-    Return the average of a list
-    """
-    return sum(lst) / len(lst)
 
 if __name__ == '__main__':
     game_from_browser('lvl2.png')
--- image_filtering_test.py
+++ image_filtering_test.py
@@ -1,125 +1,230 @@
 """
 image_filtering_test
 Author: Neil Balaskandarajah
-Created on: 24/06/2020
-Test the image filtering capabilities
+Created on: 26/06/2020
+Test filters and functions from image_filtering
 """
 import cv2
-from os import listdir
-from copy import deepcopy
-import image_filtering
 import numpy as np
+from image_filtering import *
+from os import listdir
 
-#Utility functions
+def window():
+    #Create the image and convert it to HSV
+    img = cv2.imread('tests//lvl6.png', cv2.IMREAD_COLOR)
+    img = scale_image(img, 0.5)
+    orig = deepcopy(img)
 
-def _show_images(images):
-    """
-    Show all of the images
-    """
-    for image in images:
-        cv2.imshow('img', image)
-        cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #Set up the window
+    win_name = 'filtering_gui'
+    cv2.namedWindow(win_name)
 
-def _show_images_before_after(before, after):
-    """
-    Show a pair of images to demonstrate the effect of the filter
-    """
-    for bef, aft in zip(before, after):
-        cv2.imshow('before', bef); cv2.imshow('after', aft)
-        cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # Trackbars (name, min, max, [start value])
+    trackbars = [
+        ['Bottom H', 0, 255],
+        ['Bottom S', 0, 255],
+        ['Bottom V', 0, 255, 248],
+        ['Top H', 0, 255, 255],
+        ['Top S', 0, 255, 255],
+        ['Top V', 0, 255, 255],
+        ['Erosion', 1, 20]
+    ]
+    trackbar_names = [trackbar[0] for trackbar in trackbars]  #Just the names
 
-def _display_single_image(image):
-    """
-    Display a single image to the screen
-    """
-    cv2.imshow('img', image);
-    cv2.waitKey(0);
-    cv2.destroyAllWindows()
+    #Add the trackbars to the window
+    for trackbar in trackbars:
+        def nothing(x): pass
+        cv2.createTrackbar(trackbar[0], win_name, trackbar[1], trackbar[2], nothing)
+        if len(trackbar) > 3: cv2.setTrackbarPos(trackbar[0], win_name, trackbar[3])
 
-def _display_sequential(images):
-    """
-    Display a list of images one by one
-    """
-    for i,img in enumerate(images):
-        cv2.imshow('img', img)
-        cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    values = "don\'t press p when window starts".split()
+    while 1:
+        k = cv2.waitKey(10) & 0xFF
+        if k == 27:
+            break
 
-def _display_parallel(images):
+        #Print the HSV boundaries as python variables if p is pressed
+        elif k == ord('p'):
+            s1 = 'lower_HSV = np.array([{}, {}, {}])'.format(*values[:3])
+            s2 = 'upper_HSV = np.array([{}, {}, {}])'.format(*values[3:6])
+            print("{}\n{}\n".format(s1, s2))
+
+        #Print the HSV boundaries as arguments
+        elif k == ord('a'):
+            print("{}, {}".format(values[:3], values[3:]))
+
+        #Move one image up in list
+        elif k == ord('+'):
+            pass
+        #Move one image back in list
+        elif k == ord('-'):
+            pass
+
+        #Set the threshold boundaries to the trackbar positions
+        values = [cv2.getTrackbarPos(name, win_name) for name in trackbar_names]
+        mask = filter_bg(orig, lower=values[:3], upper=values[3:6], e=0)
+
+        # mask = cv2.erode(mask, np.ones((values[6], values[6]), np.uint8))
+        mask = cv2.morphologyEx(mask, cv2.MORPH_ERODE, np.ones((values[6], values[6]), np.uint8))
+
+        img = cv2.bitwise_and(orig, orig, mask=mask)
+
+        cv2.imshow('filtered', img)
+
+def filter_bg(image, lower=(16,0,0), upper=(255,255,255), e=14):
     """
-    Display a list of images all at once
+    Filter the background out for an image given lower and upper bounds
+    Convert to HSV -> threshold in range -> morphologically close
     """
-    for i,img in enumerate(images):
-        cv2.imshow('img{}'.format(i+1), img)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    image = cv2.inRange(image, np.array(lower), np.array(upper))
+    image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, np.ones((e, e), np.uint8))
+    return image
+
+def get_stack_bounds(image):
+    """
+    Get the bounds of each stack in that image
+    """
+    # Backgroundless
+    no_bg = filter_bg(image)
+
+    # Get the bounding box for each stack
+    contours, _ = cv2.findContours(no_bg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    return [cv2.boundingRect(c) for c in contours]
+
+def get_click_locations(stack_bounds):
+    """
+    Return the click locations for each stack
+    """
+    STACK_LABELS = 'ABCDEFGH'; stacks = dict()
+    for i, r in enumerate(stack_bounds):
+        x,y,w,h = r
+        stacks[STACK_LABELS[i]] = (x+w//2, y+h//2)
+    return stacks
+
+def get_stack_images(image):
+    """
+    Return the subimage of each stack in the image
+    """
+    return [image[y:y + h, x:x + w] for x, y, w, h in get_stack_bounds(image)]
+
+#HAVE TO CHANGE TO USING ENTIRE GAME IMAGE TO BE ABLE TO LABEL, THIS IS POC
+#Consistently splits green hoops into two pieces
+#Not creating one contour per hoop consistently
+def get_game_stack(stack_image):
+    """
+    Get the game version of a stack from an image of one
+    """
+    orig = deepcopy(stack_image)
+
+    #Filter out the background and erode it to separate the hoops
+    mask = filter_bg(stack_image, lower=[0,0,248], upper=[255,255,255], e=0)
+    e = 4; mask = cv2.erode(mask, np.ones((e,e), np.uint8))
+
+    stack_image = cv2.bitwise_and(stack_image, stack_image, mask=mask)
+    stack_image = cv2.medianBlur(stack_image, 3)
+
+    #Find each hoop
+    contours, _ = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(orig, contours, -1, 255, 2)
+    orig = cv2.putText(orig, str(len(contours)), (10, 30), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+
+    cv2.imshow('no_bg', stack_image)
+    cv2.imshow('orig', orig)
     cv2.waitKey(0); cv2.destroyAllWindows()
 
-#Test cases
+    return len(contours)  #change to model game
 
-def _test_filter_bg(images):
+#--------------Tests--------------#
+
+def _test_background_filtering(images):
     """
-    Test filter_bg from image filtering
+    Apply the background filter on each image and display it
     """
-    return [image_filtering.filter_bg(img) for img in images]
+    for image in images:
+        cv2.imshow('original', image)
+        image = filter_bg(image)
+
+        cv2.imshow('filter_bg', image)
+        cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 def _test_find_stacks(images):
     """
-    Test find_stacks from image filtering
+    Find the stacks in each image and display them
+    """
+    #Show the stack bounds for all images
+    for image in images:
+        cv2.imshow('original', image)
+        stacks = get_stack_bounds(image)
+
+        #Draw the bounds of each stack and the number of stacks
+        image = cv2.putText(image, str(len(stacks)), (10,30), cv2.FONT_HERSHEY_DUPLEX, 1, (0,255,0), 2, cv2.LINE_AA)
+        for stack in stacks:
+            image = draw_rect(image, stack)
+        cv2.imshow('stacks', image)
+        cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def _test_click_locations(images):
+    """
+    Find the click locations in each image and display them
+    """
+    #Show the click locations for all images
+    for image in images:
+        cv2.imshow('original', image)
+        stack_bounds = get_stack_bounds(image)
+        clicks = get_click_locations(stack_bounds)
+
+        #Draw the points at the center of their stacks and the number of points
+        image = cv2.putText(image, str(len(clicks)), (10, 30), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+        for click in clicks.values():
+            image = cv2.circle(image, click, 5, (0,0,0), -1)
+        cv2.imshow('clicks', image)
+        cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def _test_stack_images(images):
+    """
+    Create all the sub-images for each stack and display them
     """
     for image in images:
-        rects = image_filtering.find_stacks(image).values()
-        for x,y,w,h in rects:
-            image = cv2.rectangle(image, (x,y), (x+w,y+h), 255, 3)
-    return images
+        cv2.imshow('original', image)
+        for i,stack_image in enumerate(get_stack_images(image)):
+            cv2.imshow('stack{}'.format(i), scale_image(stack_image, 1))
+        cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-def _test_create_stack(images):
+def _test_game_stacks(images):
     """
-    Test create_stack from image filtering
-    """
-    for image in images:
-        return
-
-def _test_stack_subimages(images):
-    """
-    Test stack_subimages from image filtering
+    Create the model game stacks and print them
     """
     for image in images:
         cv2.imshow('image', image)
-        #Get the bounds of each stack
-        stack_bounds = image_filtering.find_stacks(image).values()
 
-        #Get stack subimages and scale up
-        stacks = image_filtering.stack_subimages(image, stack_bounds)
-        stacks = [image_filtering.scale_image(stack, 2) for stack in stacks]
-        # stacks.insert(0, image)
+        #Get the images of each stack
+        stack_images = get_stack_images(image)
 
-        for stack in stacks:
-            cv2.imshow('stack', stack); cv2.waitKey(0)
+        #Print the game's version of the stack given the image
+        for stack_image in stack_images:
+            stack_image = scale_image(stack_image, 3)
+            get_game_stack(stack_image)
 
-def _test_unique_colors(image):
-    """
-    Test the unique colors function from image filtering
-    """
-    _display_single_image(image)
-    stack_bounds = image_filtering.find_stacks(image)
-    stacks = image_filtering.stack_subimages(image, stack_bounds)
+"""
+Findings
+16,0,0 - 255,255,239 filters out to get individual stacks
+18,0,0 - 255,137,235 gets rid of holders
+[0, 0, 0], [255, 255, 219] gets rid of shadow and bg, keeps colors (morph transformation after to solidify)
+16,0,0 - 255,255,255 gets rid of bg
+"""
 
+#Get the images
+DIR = 'tests//'
+images = [scale_image(cv2.imread(DIR+file, cv2.IMREAD_COLOR), 0.5) for file in listdir(DIR)]
 
-
-#Main Program
-
-if __name__ == '__main__':
-    # Load all the images and scale them down
-    DIR = 'tests//'
-    images = [cv2.imread(DIR+file, cv2.IMREAD_COLOR) for file in listdir(DIR)]
-    images = [image_filtering.scale_image(image, 0.5) for image in images]
-
-    #Copy of images for displaying after
-    orig = deepcopy(images)
-
-    #Run tests
-    # _show_images_before_after(orig, _test_filter_bg(images))  #Seems to be filtering red and orange out more
-    # _show_images_before_after(orig, _test_find_stacks(images))  #Finds stacks properly
-    # _show_images_before_after(orig, _test_create_stack(images))  #Find the colors in the stack
-    _test_stack_subimages(images)
+# window()
+# _test_background_filtering(images)
+# _test_find_stacks(images)
+# _test_click_locations(images)
+# _test_stack_images(images)
+_test_game_stacks(images)
